d1228515ab6fc975c28dc15b0bbd7a2f0802ea86
Kevin TSI <kevtsiii@gmail.com>
2021-10-26 15:49:36 +0200

    TDD4 testings

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -1,3 +1,20 @@
+from app import main
+
+def test_ping(test_app):
+    # Given
+    # test_app
+    
+    #When
+    response = test_app.get("/ping")
+    
+    #Then
+    assert response.status_code == 200
+    assert response.json() == {
+        "environment": "dev",
+        "ping": "pong!",
+        "testing": True
+        }
+
 # if a class is used, it must begin with Test
 class TestFoo:
     # test functions must begin with test_
