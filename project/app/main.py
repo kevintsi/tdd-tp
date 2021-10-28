ad33e61ed52e1aa6ea88ab54e5772e4f47f56d89
Kevin TSI <kevtsiii@gmail.com>
2021-10-28 13:52:46 +0200

    Update main.yml

--- project/app/main.py
+++ project/app/main.py
@@ -12,14 +12,6 @@ log = logging.getLogger("uvicorn")
 
 def create_application() -> FastAPI:
     application = FastAPI()
-    register_tortoise(
-        application,
-        db_url=os.environ.get("DATABASE_URL"),
-        modules={"models": ["app.models.tortoise"]},
-        generate_schemas=False,
-        add_exception_handlers=True,
-    )
-
     application.include_router(ping.router)
     application.include_router(
         summaries.router, prefix="/summaries", tags=["summaries"]
